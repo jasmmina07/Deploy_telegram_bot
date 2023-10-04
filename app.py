8c793a835e9027df6a415264a9a7a0e7660c2fd3
Jasmina <jasminatarmasheva007@gmail.com>
2023-10-04 16:08:16 +0500

    Done

---
+++ app.py
@@ -0,0 +1,27 @@
+import os 
+from flask import Flask, request
+import telegram
+
+TOKEN='6442939025:AAFj8oC6_RTuzIarwoWbbGpL8-GrVJ2-s-g'
+
+app = Flask(__name__)
+
+@app.route('/',methods=["POST", "GET"])
+def main():
+    if request.method == "POST":
+        update = request.get_json()
+
+        text = update['message']['text']
+        chat_id = update['message']['chat']['id']
+
+        bot = telegram.Bot(TOKEN)
+
+        bot.sendMessage(chat_id, text)
+
+        return 'Ok'
+    else:
+        return {"result": "Only post request"}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-a=0
-print(a)
+from telegram import*
+from flask import request, Flask
+
+TOKEN="6442939025:AAFj8oC6_RTuzIarwoWbbGpL8-GrVJ2-s-g"
+bot=Bot(TOKEN)
+url="https://jasmina.pythonanywhere.com/webhook/"
+
+print(bot.set_webhook(url))
